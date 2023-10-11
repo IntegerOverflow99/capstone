fb8f4f5348fa94167bf567d07883faa7d26fc362
Brandon Lowe <lcbdot@gmail.com>
2023-10-11 17:57:54 -0400

    missed barrel file

--- utils/src/entities/index.ts
+++ utils/src/entities/index.ts
@@ -3,7 +3,16 @@ import { Model } from 'objection';
 import config from '../objection-utils/knexconfig';
 
 export * from './TableNames';
+
+export * from './AccessLog';
+export * from './Audio';
+export * from './CustomTag';
+export * from './Favorite';
+export * from './Media';
+export * from './Photo';
+export * from './TaggedMedia';
 export * from './User';
+export * from './Video';
 
 const knex = Knex(config);
 
