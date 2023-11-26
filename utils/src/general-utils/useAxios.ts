f8a88c412bc0b609a94d1b239a0ae647d8ce8a45
Brandon Lowe <lcbdot@gmail.com>
2023-11-26 11:35:59 -0500

    rolling back, whoops

--- utils/src/general-utils/useAxios.ts
+++ utils/src/general-utils/useAxios.ts
@@ -5,11 +5,12 @@ export function useAxios() {
   //create an instance of axios pointed at the api on the same address as this webserver app, on port 3000 rather than the port the webserver is running on
   const { current: instance } = useRef(
     axios.create({
-      baseURL: 'https://localhost:4200',
+      baseURL:
+        process.env['NODE_ENV'] === 'production'
+          ? 'https://localhost:4200'
+          : 'http://localhost:4200',
     })
   );
-
-  console.log(process.env);
 
   return instance;
 }
