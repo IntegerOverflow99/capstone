ac00b8026b139cf4978789b4fd882b04776993a0
Brandon Lowe <lcbdot@gmail.com>
2023-11-26 11:15:42 -0500

    remove middleware logging, useAxios modified for nextjs api proxy

--- apps/capstone-frontend/middleware.ts
+++ apps/capstone-frontend/middleware.ts
@@ -7,19 +7,15 @@ export const middleware = async (req: NextRequest) => {
   const res = NextResponse.next();
   const { user } = await getIronSession(req, res, sessionOptions);
 
-  console.log(req.nextUrl.pathname);
+  // console.log(req.nextUrl.pathname);
 
   if (req.nextUrl.pathname === '/') {
-    console.log('index page');
     return res;
   } else if (req.nextUrl.pathname.startsWith('/api/login')) {
-    console.log('api login');
     return res;
   } else if (req.nextUrl.pathname.startsWith('/api') && !user) {
-    console.log('api unauthorized access');
     return NextResponse.json({ msg: 'Unauthorized' }, { status: 401 });
   } else if (!user && !req.nextUrl.pathname.startsWith('/_next')) {
-    console.log('redirecting');
     return NextResponse.redirect(new URL('/', req.url), { status: 302 });
   }
 
--- utils/src/general-utils/useAxios.ts
+++ utils/src/general-utils/useAxios.ts
@@ -4,7 +4,7 @@ import { useRef } from 'react';
 export function useAxios() {
   //create an instance of axios pointed at the api on the same address as this webserver app, on port 3000 rather than the port the webserver is running on
   const { current: instance } = useRef(
-    axios.create({ baseURL: 'http://localhost:3000' })
+    axios.create({ baseURL: 'http://localhost:4200' })
   );
 
   return instance;
