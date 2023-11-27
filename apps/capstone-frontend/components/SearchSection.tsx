7b3834d695be3a602c4245e62019228394a6875c
Brandon Lowe <lcbdot@gmail.com>
2023-11-27 18:20:35 -0500

    deprecating table for datagrid (free pagination, w)

--- apps/capstone-frontend/components/SearchSection.tsx
+++ apps/capstone-frontend/components/SearchSection.tsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import {
-  TableContainer,
-  TableCell,
-  TableHead,
-  TableRow,
-  Table,
-  Paper,
-  Typography,
-  Box,
-  SxProps,
-  TableBody,
-} from '@mui/material';
+import { Typography, Box, SxProps, TableBody } from '@mui/material';
 import { camelToCapsAndSpaces } from '@capstone/utils/general';
 import {
   IAudioJSONModel,
@@ -18,6 +7,7 @@ import {
   IVideoJSONModel,
 } from '@capstone/utils/types';
 import { useRouter } from 'next/router';
+import { DataGrid } from '@mui/x-data-grid';
 
 type SearchSectionProps = {
   title?: string;
@@ -36,44 +26,27 @@ const SearchSection = (props: SearchSectionProps) => {
       {data.length === 0 && (
         <Typography variant="h4">No Results Found!</Typography>
       )}
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              {Object.keys(data[0] || {}).map((key, idx, arr) => {
-                return (
-                  <React.Fragment key={idx}>
-                    <TableCell>
-                      <strong>{camelToCapsAndSpaces(key)}</strong>
-                    </TableCell>
-                  </React.Fragment>
-                );
-              })}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((row, idx) => {
-              return (
-                <TableRow
-                  hover
-                  key={idx}
-                  onClick={() => {
-                    router.push(`${navigatesTo}/${row.id}`);
-                  }}
-                >
-                  {Object.values(row).map((value, idx) => {
-                    return (
-                      <React.Fragment key={idx}>
-                        <TableCell>{value}</TableCell>
-                      </React.Fragment>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <DataGrid
+        columns={Object.keys(data[0] || {}).map((key, idx, arr) => {
+          return {
+            field: key,
+            headerName: camelToCapsAndSpaces(key),
+            width: 200,
+          };
+        })}
+        rows={data as any}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              page: 0,
+              pageSize: 5,
+            },
+          },
+        }}
+        onRowClick={({ row }) => {
+          router.push(`${navigatesTo}/${row.id}`);
+        }}
+      />
     </Box>
   );
 };
